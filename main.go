c82aa96cc504d7032c7b12668f5b20c94ab3f1a9
kevinliyanto <kevinliyanto@hotmail.com>
2024-02-14 02:43:17 +1100

    add resource with duration

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	r1 := GenerateRandomResource()
 	fmt.Println(r1.resource)
 
-	r2 := GenerateRandomResource()
+	r2 := GenerateEmptyResource()
 	fmt.Println(r2.resource)
 
-	fmt.Println("Adding resource from r2 to r1")
+	fmt.Println("Adding resource from r2 to r1 after 10 seconds")
 
-	r1.AddMaterial(r2.resource)
+	duration, _ := time.ParseDuration("10s")
+	time.Sleep(duration)
+
+	r1.AddResource(r2)
 	fmt.Println(r1.resource)
 }
--- types.go
+++ types.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"fmt"
 	math_rand "math/rand"
+	"time"
 )
+
+// type Iron float64
+// type Copper float64
+// type Coal float64
+// type Water float64
 
 type Material struct {
 	Iron   float64
@@ -12,11 +19,30 @@ type Material struct {
 }
 
 type Resource struct {
-	resource      *Material
-	resourceLimit *Material
+	resource *Material
+	// Rate per 1000 ms or time.ParseDuration("1s")
+	resourceRate     *Material
+	resourceLimit    *Material
+	timeLastCaptured time.Time
+}
+
+func GenerateEmptyResource() *Resource {
+	return &Resource{
+		resource:         &Material{},
+		resourceRate:     &Material{},
+		resourceLimit:    &Material{},
+		timeLastCaptured: time.Now(),
+	}
 }
 
 func GenerateRandomResource() *Resource {
+	defaultRate := Material{
+		Iron:   10.0,
+		Copper: 10.0,
+		Coal:   10.0,
+		Water:  5.0,
+	}
+
 	defaultLimit := Material{
 		Iron:   80000.0,
 		Copper: 80000.0,
@@ -31,13 +57,46 @@ func GenerateRandomResource() *Resource {
 			Coal:   defaultLimit.Coal * math_rand.Float64(),
 			Water:  defaultLimit.Water * math_rand.Float64(),
 		},
-		resourceLimit: &defaultLimit,
+		resourceRate:     &defaultRate,
+		resourceLimit:    &defaultLimit,
+		timeLastCaptured: time.Now(),
 	}
 }
 
-func (r *Resource) AddMaterial(m *Material) {
+func (r *Resource) calculateOnResourceRate(timeOnCalculation *time.Time) {
+	durationSinceLastCalculation := timeOnCalculation.Sub(r.timeLastCaptured)
+
+	fmt.Println("Duration since last calculation", durationSinceLastCalculation)
+
+	resourceDiff := r.resourceRate.calculateMaterialDiff(&durationSinceLastCalculation)
+	r.addMaterial(resourceDiff)
+}
+
+func (r *Resource) addMaterial(m *Material) {
 	r.resource.Iron += m.Iron
 	r.resource.Copper += m.Copper
 	r.resource.Coal += m.Coal
 	r.resource.Water += m.Water
+}
+
+func (resourceRate *Material) calculateMaterialDiff(d *time.Duration) *Material {
+	duration := float64(d.Milliseconds()) / 1000
+
+	return &Material{
+		Iron:   resourceRate.Iron * duration,
+		Copper: resourceRate.Copper * duration,
+		Coal:   resourceRate.Coal * duration,
+		Water:  resourceRate.Water * duration,
+	}
+}
+
+func (r *Resource) AddResource(ext *Resource) {
+	timeOnCalculation := time.Now()
+
+	r.calculateOnResourceRate(&timeOnCalculation)
+
+	r.resource.Iron += ext.resource.Iron
+	r.resource.Copper += ext.resource.Copper
+	r.resource.Coal += ext.resource.Coal
+	r.resource.Water += ext.resource.Water
 }
